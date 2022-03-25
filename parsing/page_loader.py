1b9e43fa9622e75e85968c239a00711a98175b72
Andrey Putilin <regresscheck@gmail.com>
2022-03-25 16:53:52 -0700

    basic caching

--- parsing/page_loader.py
+++ parsing/page_loader.py
@@ -1,4 +1,12 @@
-import requests
+from datetime import timedelta
+from requests_cache import CachedSession
+
+session = CachedSession(
+    'basic_cache',
+    backend='filesystem',
+    use_cache_dir=True,
+    expire_after=timedelta(days=1)
+)
 
 
 class PageLoader():
@@ -7,8 +15,7 @@ class PageLoader():
         pass
 
     def get_url(self, url):
-        # TODO: cache results somewhere
-        response = requests.get(url)
+        response = session.get(url)
         # TODO: proper error/redirect handling
         assert response.status_code == 200
         return response.text
