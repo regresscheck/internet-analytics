b734fe8905484e381bbdf474ce2ab3c7fa81611d
Andrey Putilin <regresscheck@gmail.com>
2022-03-27 16:39:25 -0700

    added entity domain assert

--- parsing/entity_extractor_base.py
+++ parsing/entity_extractor_base.py
@@ -1,9 +1,12 @@
 import abc
+from urllib.parse import urlparse
 from parsing.page_loader import PageLoader
 
 
 class EntityExtractorBase(abc.ABC):
     def __init__(self, url) -> None:
+        domain = urlparse(url).netloc
+        assert domain == self.get_supported_domain()
         self.url = url
 
     @abc.abstractmethod
